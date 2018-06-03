6e0564132c7d7a128e1f895bfaad1aa2a1d16323
Hitesh Ahuja <hiteshah@amdocs.com>
2018-06-03 14:16:10 +0530

    Initial Version of the project

---
+++ app/Http/Controllers/ProductCategory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductCategory extends Controller
+{
+    //
+	
+	public function all()
+	{
+		return "all";	
+	}
+	
+	public function insert()
+	{
+		return "insert success";
+	}
+	
+	public function delete()
+	{
+		return "delete success";		
+	}
+	
+	public function update()
+	{
+		return "update success";	
+	}
+}
